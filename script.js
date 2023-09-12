d6211ffea3a40de33798bc850e99ec6962953073
citdrhs <123655845+citdrhs@users.noreply.github.com>
2023-09-12 12:56:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*Used for the Navigation Bar*/
+var navLinks = document.getElementById("navLinks");
+
+function hideMenu(){
+    navLinks.style.visibility = "hidden";
+}
+
+function showMenu(){
+    navLinks.style.visibility = "visible";
+}
+
